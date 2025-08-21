e7baa4928defe6ed7ba0452e4dfa74f9781ce0cc
Jad Nehmeh <88036369+jnehmeh87@users.noreply.github.com>
2025-08-21 14:51:50 +0200

    feat: Fix local email verification

--- time_stamp/settings.py
+++ time_stamp/settings.py
@@ -132,6 +132,11 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
+
+
+# Use console backend for emails during development
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Activate Django-Heroku.
