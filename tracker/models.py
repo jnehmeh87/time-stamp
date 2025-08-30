5f3fbc96665049a8e5ed2e48fe05293e492f6df9
Jad Nehmeh <88036369+jnehmeh87@users.noreply.github.com>
2025-08-30 15:55:59 +0200

    Add migrations for tracker app

--- tracker/models.py
+++ tracker/models.py
@@ -15,8 +15,8 @@ CATEGORY_CHOICES = [
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     country = CountryField(blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True)
-    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=255, blank=True, default='')
+    phone_number = models.CharField(max_length=20, blank=True, default='')
 
     def __str__(self):
         return f'{self.user.username} Profile'
